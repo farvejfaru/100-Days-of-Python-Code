cd72136ca6edf171e14bcc84524c69c95a446098
farvej faru <103867390+farvejfaru@users.noreply.github.com>
2024-12-24 14:33:22 +0530

    Update main.py

--- Day 3/main.py
+++ Day 3/main.py
@@ -1,93 +1,22 @@
-print('''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-
-first = input("Left or right?\nType Right/Left: ").lower()
-if first == "right":
-  print('''                      -     =    .--._
-                - - ~_=  =~_- = - `.  `-.
-              ==~_ = =_  ~ -   =  .-'    `.
-            --=~_ - ~  == - =   .'      _..:._
-           ---=~ _~  = =-  =   `.  .--.'      `.
-          --=_-=- ~= _ - =  -  _.'  `.      .--.:
-            -=_~ -- = =  ~-  .'      :     :    :
-             -=-_ ~=  = - _-`--.     :  .--:    D
-               -=~ _=  =  -~_=  `;  .'.:   ,`---'@
-             --=_= = ~-   -=   .'  .'  `._ `-.__.'
-            --== ~_ - =  =-  .'  .'     _.`---'
-           --=~_= = - = ~  .'--''   .   `-..__.--.
-             --==~ _= - ~-=  =-~_-   `-..___(  ===;
-          --==~_==- =__ ~-=  - -    .'       `---'
-  ''')
-  print("Sonic got the treasure before you, try again.")
-elif first == 'left':
-  print('''
-   _                                                           
-  | |                                                          
-  | |_ _ __ ___  __ _ ___ _   _ _ __ ___ _ __ ___   __ _ _ __  
-  | __| '__/ _ \/ _` / __| | | | '__/ _ \ '_ ` _ \ / _` | '_ \ 
-  | |_| | |  __/ (_| \__ \ |_| | | |  __/ | | | | | (_| | |_) |
-  \__|_|  \___|\__,_|___/\__,_|_|  \___|_| |_| |_|\__,_| .__/ 
-                                                        | |    
-                                                        |_| 
-  ''')
-  print("Nice, you made it to the next level!")
-
-  second = input("Your map shows that you need to get to Treasure Island, you can wait to board a ship or swim accross the sea, pick one.\nType Swim/Wait: ").lower()
-  if second == "swim":
-    print('''
-                    (`.
-                    \ `.
-                      )  `._..---._
-    \`.       __...---`         o  )
-    \ `._,--'           ,    ___,'
-      ) ,-._          \  )   _,-'
-    /,'    ``--.._____\/--''
-        ''')
-    print("Unfortunately, you were eaten by a Great White Shark, try again.")
-  elif second == "wait":
-    print("Nice, you made it to the next level, you're pretty good at this!")
-    print ("Welcome to:")
-    print ('''
-     _                                     _     _                 _ 
-    | |                                   (_)   | |               | |
-    | |_ _ __ ___  __ _ ___ _   _ _ __ ___ _ ___| | __ _ _ __   __| |
-    | __| '__/ _ \/ _` / __| | | | '__/ _ \ / __| |/ _` | '_ \ / _` |
-    | |_| | |  __/ (_| \__ \ |_| | | |  __/ \__ \ | (_| | | | | (_| |
-    \__|_|  \___|\__,_|___/\__,_|_|  \___|_|___/_|\__,_|_| |_|\__,_|
-    ''')
-
-    third = input("Now that you've made it to Treasure Island, you can dig or search the cave. \n Type Dig/Cave: ").lower()
-    if third == "dig":
-      print("You've found the treasure, you win!")
-    elif third == "cave":
-      print('''
-      _                     
-      | |                    
-      | |__   ___  __ _ _ __ 
-      | '_ \ / _ \/ _` | '__|
-      | |_) |  __/ (_| | |   
-      |_.__/ \___|\__,_|_|   
-                  ''')
-      print("You were eaten by a bear, game over.")
+print("Welcome to Tresure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input(
+    'You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+    choice2 = input(
+        'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    if choice2 == "wait":
+        choice3 = input(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
